241f76a388591351c582572a5116728146e4f49c
Iman <1315090+iheidari@users.noreply.github.com>
2019-10-27 15:35:17 -0700

    Feature/kdls 1 (#2)
    * setup enviroment varaiables
    
    * fix package.json

---
+++ config/environments.js
@@ -0,0 +1,7 @@
+module.exports = {
+  environment: process.env.NODE_ENV,
+  port: process.env.PORT,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbLink: process.env.DB_LINK
+};
