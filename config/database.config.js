e02cd547f05692a349103268f1fba5639db65c5c
sarabahrani <sara.bahrani@gmail.com>
2019-11-03 19:08:28 -0800

    fix typo

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 const { dbProtocol, dbUser, dbPassword, dbLink } = require("./environments");
 
 module.exports = {
-  url: `${dbProtocol}://${dbUser}:${dbPassword}${dbLink ? "@" + dblink : ""}`
+  url: `${dbProtocol}://${dbUser}:${dbPassword}${dbLink ? "@" + dbLink : ""}`
 };
